b2e8a2f40fa1447cbe4c8aefb1cd2b995274889e
armanengin <a.enginsucu@gmail.com>
2023-12-02 22:09:23 -0800

    added properties by address function

--- controller/zillowController.js
+++ controller/zillowController.js
@@ -27,6 +27,31 @@ export async function searchProperties(req, res) {
     }
 }
 
+async function getPropertiesByAddress(address) {
+    if (!address) {
+        throw new Error('Address is required');
+    }
+
+    const options = {
+        method: 'GET',
+        url: 'https://zillow56.p.rapidapi.com/search',
+        params: { location: address },
+        headers: {
+            'X-RapidAPI-Key': '302e8cc6f5mshb301d1d596b2433p13baeejsn8720ea500b8f', // Replace with your actual API key
+            'X-RapidAPI-Host': 'zillow56.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await axios.request(options);
+        return response.data; // Return the property data
+    } catch (error) {
+        console.error(error);
+        throw new Error('Failed to fetch property data');
+    }
+}
+
+
 // Function to get nearby schools based on a Zillow Property ID (zpid)
 export async function nearbySchools(req, res) {
     const zpid = req.query.zpid; // Extract the zpid from query parameters
